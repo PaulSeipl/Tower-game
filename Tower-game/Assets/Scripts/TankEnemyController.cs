4f0ec00fff87f3eb22c6f404c9b4acf0432e33a7
kittel <f.kittel@campus.lmu.de>
2020-07-12 00:05:41 +0200

    Implemented enemy movement towards the tower

---
+++ Tower-game/Assets/Scripts/TankEnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankEnemyController : MonoBehaviour {
+
+    public float speed;
+
+    private bool isMoveingRight = true;
+
+    public GameObject Tower;
+
+    void Start() {
+        if (Tower.transform.position.x < transform.position.x) {
+            isMoveingRight = false;
+        } else {
+            isMoveingRight = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoveingRight) {
+            transform.Translate(2 * Time.deltaTime * speed, 0,0);
+        } else {
+            transform.Translate(-2 * Time.deltaTime * speed, 0,0);
+        }
+    }
+}
