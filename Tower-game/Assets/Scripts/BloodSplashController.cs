f8f25a97e9bdcc0f0185b0600137d75016d440e0
kittel <f.kittel@campus.lmu.de>
2020-07-13 15:18:44 +0200

    added the player, enemies and a weapon

---
+++ Tower-game/Assets/Scripts/BloodSplashController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSplashController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke("DestroyObject", 3);
+        Debug.Log("Ich wurde erstellt");
+    }
+
+    private void DestroyObject() {
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Tower-game/Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+
+    public GameObject normalEnemy;
+    public GameObject tankEnemy;
+    public GameObject fastEnemy;
+
+    private List<GameObject> enemyList = new List<GameObject>();
+    Vector2 whereToSpawn;
+    public float spawnRate = 2;
+    float nextSpawn = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyList.Add(normalEnemy);
+        enemyList.Add(fastEnemy);
+        enemyList.Add(tankEnemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Time.time > nextSpawn) {
+            nextSpawn = Time.time + spawnRate;
+            whereToSpawn = new Vector2(transform.position.x, transform.position.y);
+
+            int enemyIndex = (int) Random.Range(0, 3.1f);
+
+            GameObject enemy = enemyList[enemyIndex];
+
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+
+    }
+}
