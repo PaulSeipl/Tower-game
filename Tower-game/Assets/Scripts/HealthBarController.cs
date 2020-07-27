bbc7eb817b8a8c7144349a71a43724ae9fc0dcaa
kittel <f.kittel@gmx.net>
2020-07-27 15:35:27 +0200

    added HealthBar

--- Tower-game/Assets/Scripts/HealthBarController.cs
+++ Tower-game/Assets/Scripts/HealthBarController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class HealthBarController : MonoBehaviour
 {
     private static Transform redBar;
+    private static float maxHealth;
     private void Start()
     {
         redBar = transform.Find("Bar");
+        maxHealth = redBar.localScale.x;
     }
 
     public static void SetSize(float sizeNormalized) {
-        //redBar.localScale = new Vector3(sizeNormalized, 1f);
-
-        Debug.Log("Ich bin drin");
+        Vector3 newScale = redBar.localScale;
+        newScale.x = maxHealth * sizeNormalized;
+        redBar.localScale = newScale;
     }
 }
